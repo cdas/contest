1ca18ab8a3545d9398b4aa285b151ceafef6f83a
Chris Daseking <cdas@habmalnefrage.de>
2013-04-29 21:12:32 +0200

    first try to respond on plista api

--- contest.rb
+++ contest.rb
@@ -4,16 +4,21 @@ require 'json'
 
 post "/" do
 	msg = JSON.parse(request.body.read)
-
+	puts msg.to_s
 
 	if msg.has_key? 'error' 
 		return
 	elsif msg.has_key? 'msg'
 		if msg["msg"] == "impression"
-#			team_id = msg[:config][:team]
-			item_id = msg[:item][:id]
-			version = msg[:version]
-			result = {:msg => "result", :items => [{:id => item_id}], :team => {:id => team_id}, :version => version}
+			# team_id = msg[:config][:team]
+			# if msg[:item][:id]
+				# item_id = msg[:item][:id]
+			# end
+			# if msg[:version]
+				# version = msg[:version]
+			# end
+			#result = {:msg => "result", :items => [{:id => item_id}], :team => {:id => team_id}, :version => version}
+			result = {:msg => "result"}
 			return result.to_json
 		elsif msg["msg"] == "feedback"
 			return result.to_json
