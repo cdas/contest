c3c0d62066bc229651a68533ee11f12e72f847c4
Chris Daseking <cdas@habmalnefrage.de>
2013-05-03 00:10:49 +0200

    domain specific recommendation of the former item implemented

--- contest.rb
+++ contest.rb
@@ -2,7 +2,23 @@ require 'rubygems'
 require 'sinatra'
 require 'json'
 
-@@former_item = 1
+@@domainlist = Hash.new
+
+class Domain
+	def initialize (domain_id)
+		@id = domain_id
+		@former_itemid = "1"
+	end
+	
+	def save itemid
+		@former_itemid = itemid
+	end
+	
+	def give_itemid
+		@former_itemid
+	end
+end
+
 
 get '/*' do
 	return "Try POST!"
@@ -13,17 +29,29 @@ post '/*' do
 
 	def result msg
 		if config = msg["config"]
-			team_id = config["team"]
+			team_id = config["team"]["id"]
 		end
-		if item = msg["item"]
-			logger.info item
-			item_id = @@former_item 
-			@@former_item = item["id"]
+		if domain_id = msg["domain"]["id"] 
+			if not @@domainlist.has_key? domain_id
+				logger.info "domain unbekannt"
+				@@domainlist[domain_id] = Domain.new(domain_id)
+			else
+				logger.info "domain bekannt" + domain_id.to_s
+			end
+		end
+		if item_id = msg["item"]["id"]
+ 			logger.info item_id
+			if former_itemid = @@domainlist[domain_id].give_itemid
+				@@domainlist[domain_id].save(item_id)
+				item_id = former_itemid
+			end
 		end
 		if version = msg["version"]
 		end
-		result = {:msg => "result", :items => [{:id => item_id}], :team => {:id => team_id}, :version => version}
+		
+		return {:msg => "result", :items => [{:id => item_id}], :team => {:id => team_id}, :version => version}
 		#result = std_result
+		logger.info result
 	end
 
 	logger.info "Request received"
