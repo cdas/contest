2323c927c1f106b49281564859d0bd2933d7b095
Chris Daseking <cdas@habmalnefrage.de>
2013-05-02 21:34:58 +0200

    algo recommends former item

--- contest.rb
+++ contest.rb
@@ -2,34 +2,38 @@ require 'rubygems'
 require 'sinatra'
 require 'json'
 
-def result msg
-	if msg.has_key? 'error' 
-		return
-	elsif msg.has_key? 'msg'
-		if msg["msg"] == "impression"
-			if msg[:config]
-				team_id = msg[:config][:team]
-			end
-			if item = msg[:item]
-				item_id = item[:id]
-			end
-			if msg[:version]
-				version = msg[:version]
-			end
-			# result = {:msg => "result", :items => [{:id => item_id}], :team => {:id => team_id}, :version => version}
-			result = {:msg => "result", :items => [], :version => "1.0"}
-			# result = {:msg => "result"}
-			return result.to_json
-		elsif msg["msg"] == "feedback"
-			return result.to_json
-		elsif msg["msg"] == "result"
-			return result.to_json
-		end
-	end
+@@former_item = 1
+
+get '/*' do
+	return "Try POST!"
 end
 
-post "/" do
-	logger.info "loading data"
-	
-	result(JSON.parse(request.body.read))
+post '/*' do
+	std_result = {:msg => "result", :items => [], :version => "1.0"}
+
+	def result msg
+		if config = msg["config"]
+			team_id = config["team"]
+		end
+		if item = msg["item"]
+			logger.info item
+			item_id = @@former_item 
+			@@former_item = item["id"]
+		end
+		if version = msg["version"]
+		end
+		result = {:msg => "result", :items => [{:id => item_id}], :team => {:id => team_id}, :version => version}
+		#result = std_result
+	end
+
+	logger.info "Request received"
+	msg = JSON.parse(request.env["rack.input"].read)
+	logger.info "Parsed JSON Data "
+	if msg["msg"] == "impression"
+		logger.info "impression received"
+		result(msg).to_json
+	elsif
+		std_result
+	end 
+	# further processing...
 end
